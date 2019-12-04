7d8528894505883330cad49ff109be7709d3f07b
rebecca98m <rebecca.schiavon@studenti.unipd.it>
2019-12-04 13:28:14 +0100

    Creato progetto close #1

---
+++ src/main/java/it/unipd/tos/sandwich_shop_manager/App.java
@@ -0,0 +1,13 @@
+package it.unipd.tos.sandwich_shop_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
